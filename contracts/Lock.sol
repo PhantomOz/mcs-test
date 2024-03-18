8750ff7da1ac73f9b7c8a66aa645644561e6d19d
PhantomOz <faniogor@gmail.com>
2024-03-18 15:04:25 +0100

    this commit adds the Token Contract

--- contracts/Lock.sol
+++ contracts/Lock.sol
@@ -1,7 +1,11 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.18;
 
+import {Token} from "./Token.sol";
+
 /// @title CrowdFund
 /// @author Favour Aniogor
 /// @notice A crowdfunding campaign where users can pledge and claim funds to, and claim funds from, the contract.
-contract CrowdFund {}
+contract CrowdFund {
+    Token private immutable i_crowdFundToken;
+}
---
+++ contracts/Token.sol
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.4;
+
+import {ERC20} from "openzeppelin/token/ERC20/ERC20.sol";
+
+contract Token is ERC20 {
+    constructor(
+        string memory _name,
+        string memory _symbol,
+        uint256 _totalSupply
+    ) ERC20(_name, _symbol) {
+        _mint(msg.sender, amount);
+    }
+}
