b3eda7e5b8bf28d8614e5f45bae9693968abe9fc
PhantomOz <faniogor@gmail.com>
2024-03-18 11:27:22 +0100

    this commit initialises the project

---
+++ contracts/Lock.sol
@@ -0,0 +1,7 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.18;
+
+/// @title CrowdFund
+/// @author Favour Aniogor
+/// @notice A crowdfunding campaign where users can pledge and claim funds to, and claim funds from, the contract.
+contract CrowdFund {}
