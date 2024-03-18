23a27bbfc9af20805f22423729315b157d3875f7
PhantomOz <faniogor@gmail.com>
2024-03-18 15:42:21 +0100

    this commit adds adds project struct and state variables

--- contracts/Lock.sol
+++ contracts/Lock.sol
@@ -8,7 +8,14 @@ import {Token} from "./Token.sol";
 /// @notice A crowdfunding campaign where users can pledge and claim funds to, and claim funds from, the contract.
 contract CrowdFund {
     Token private immutable i_crowdFundToken; //An ERC20 Token for crowdfunding
+    mapping(address => bool) private s_projectOwners; //A map of all the Owners
 
+    struct Project {
+        uint256 fundingGOal;
+        uint256 timeline;
+    }
+
+    ///@param _token the token address this contract will use.
     constructor(address _token) {
         i_crowdFundToken = Token(_token);
     }
