ad79b1ffade37a975e3a33a6e94670a279e6b019
PhantomOz <faniogor@gmail.com>
2024-03-19 22:52:32 +0100

    This adds some touches

--- test/CrowdFund.ts
+++ test/CrowdFund.ts
@@ -2,14 +2,10 @@ import {
   time,
   loadFixture,
 } from "@nomicfoundation/hardhat-toolbox/network-helpers";
-import { anyValue } from "@nomicfoundation/hardhat-chai-matchers/withArgs";
 import { expect } from "chai";
 import hre, { ethers } from "hardhat";
 
 describe("CrowdFund", function () {
-  // We define a fixture to reuse the same setup in every test.
-  // We use loadFixture to run this setup once, snapshot that state,
-  // and reset Hardhat Network to that snapshot in every test.
   async function deployCrowdFund() {
     const NAME = "CROWDFUND TOKEN";
     const SYMBOL = "CFT";
